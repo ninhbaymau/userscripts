55708410db988ad4b49fecf0e21981ddcc0f30c3
ninhbaymau <44390877+ninhbaymau@users.noreply.github.com>
2024-06-09 23:24:44 +0700

    Create PreventPageVisibility.js

---
+++ PreventPageVisibility.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name        PreventPageVisibility 
+// @namespace   https://github.com/IceWreck
+// @match       *://*/*
+// @run-at      document-start
+// @grant       none
+// @version     1.1
+// @author      IceWreck
+// @description Block websites from knowing if you switched tabs/windows
+
+// @downloadURL https://update.greasyfork.org/scripts/427254/PreventPageVisibility.user.js
+// @updateURL https://update.greasyfork.org/scripts/427254/PreventPageVisibility.meta.js
+// ==/UserScript==
+
+// This userscript blocks the page visibility API and to some extent the old blur/focus APIs.
+
+let events_to_block = [
+  "visibilitychange",
+  "webkitvisibilitychange",
+  "mozvisibilitychange",
+  "hasFocus",
+  "blur",
+  "focus",
+  "mouseleave"
+]
+
+for (event_name of events_to_block) {
+  document.addEventListener(event_name, function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      event.stopImmediatePropagation();
+  }, true);
+}
+
+for (event_name of events_to_block) {
+  window.addEventListener(event_name, function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      event.stopImmediatePropagation();
+  }, true);
+}
+
+
+document.hasFocus = function () { return true; };
+document.onvisibilitychange = null;
+Object.defineProperty(document, "visibilityState", { value: "visible" });
+Object.defineProperty(document, "hidden", { value: false });
+Object.defineProperty(document, "mozHidden", { value: false });
+Object.defineProperty(document, "webkitHidden", { value: false });
+Object.defineProperty(document, "webkitVisibilityState", { value: "visible" });
