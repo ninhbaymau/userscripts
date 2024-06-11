d13d088801dbbfa64546c4600e34dc2007029f82
ninhbaymau <44390877+ninhbaymau@users.noreply.github.com>
2024-06-11 21:45:54 +0700

    Create test3.js

---
+++ test3.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name               Devtools for Mobile
+// @namespace     https://eruda.liriliri.io/
+// @description   Console for mobile browsers
+// @run-at        document-start
+// @match *://*/*
+// @grant         none
+// @noframes
+// @version 0.0.1.20211028143330
+// ==/UserScript==
+alert("cc");
