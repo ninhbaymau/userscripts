523f612ed2a8aec9e70cdd1c7c850438c9de71a4
ninhbaymau <44390877+ninhbaymau@users.noreply.github.com>
2024-06-11 23:54:49 +0700

    Update x.js

--- x.js
+++ x.js
@@ -1,11 +1,43 @@
 // ==UserScript==
-// @name        PreventPageVisibility 
-// @namespace   https://github.com/IceWreck
-// @match       *://*/*
+// @name        Prevent Page Visibility
+// @namespace   Disable Page Visibility API
+// @description Block websites from knowing if you switched tabs/windows
+// @author      @SalahDesDz
+// @version     0.1
 // @run-at      document-start
 // @grant       none
-// @version     1.1
-// @author      IceWreck
-// @description Block websites from knowing if you switched tabs/windows
+// @match       *://*/*
+// @license     MIT
 // ==/UserScript==
-let events_to_block=["visibilitychange","webkitvisibilitychange","mozvisibilitychange","hasFocus","blur","focus","mouseleave"];for(var event_name of events_to_block)document.addEventListener(event_name,(function(e){e.preventDefault(),e.stopPropagation(),e.stopImmediatePropagation()}),!0);for(var event_name of events_to_block)window.addEventListener(event_name,(function(e){e.preventDefault(),e.stopPropagation(),e.stopImmediatePropagation()}),!0);document.hasFocus=function(){return!0},document.onvisibilitychange=null,Object.defineProperty(document,"visibilityState",{value:"visible"}),Object.defineProperty(document,"hidden",{value:!1}),Object.defineProperty(document,"mozHidden",{value:!1}),Object.defineProperty(document,"webkitHidden",{value:!1}),Object.defineProperty(document,"webkitVisibilityState",{value:"visible"});
+ 
+// This userscript blocks the page visibility API and to some extent the old blur/focus APIs.
+ 
+const eventsToBlock = [
+  "visibilitychange",
+  "webkitvisibilitychange",
+  "mozvisibilitychange",
+  "hasFocus",
+  "blur",
+  "focus",
+  "mouseleave",
+];
+ 
+const preventDefaultHandler = (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  event.stopImmediatePropagation();
+};
+ 
+for (const eventName of eventsToBlock) {
+  document.addEventListener(eventName, preventDefaultHandler, true);
+}
+ 
+for (const eventName of eventsToBlock) {
+  window.addEventListener(eventName, preventDefaultHandler, true);
+}
+ 
+document.hasFocus = () => true;
+document.onvisibilitychange = null;
+Object.defineProperty(document, "visibilityState", { value: "visible" });
+Object.defineProperty(document, "hidden", { value: false });
+Object.defineProperty(document, "mozHidden", { value: false });
