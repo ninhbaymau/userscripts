14f143959111a47d527dde5d26bdbb0737f3d375
ninhbaymau <44390877+ninhbaymau@users.noreply.github.com>
2024-06-11 21:52:45 +0700

    Create x.js

---
+++ x.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        PreventPageVisibility 
+// @namespace   https://github.com/IceWreck
+// @match       *://*/*
+// @run-at      document-start
+// @grant       none
+// @version     1.1
+// @author      IceWreck
+// @description Block websites from knowing if you switched tabs/windows
+// ==/UserScript==
+
+let events_to_block = [
+  "visibilitychange",
+  "webkitvisibilitychange",
+  "mozvisibilitychange",
+  "hasFocus",
+  "blur",
+  "focus",
+  "mouseleave"
+]
+
+for (event_name of events_to_block) {
+  alert("cc");
+}
