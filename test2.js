bf9a678d37dffaba412cc4db47cb970615c9fb4a
ninhbaymau <44390877+ninhbaymau@users.noreply.github.com>
2024-06-11 21:43:57 +0700

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name               Devtools for Mobile
+// @namespace     https://eruda.liriliri.io/
+// @description   Console for mobile browsers
+// @require       https://cdn.jsdelivr.net/npm/eruda
+// @run-at        document-start
+// @match *://*/*
+// @grant         none
+// @noframes
+// @version 0.0.1.20211028143330
+// @downloadURL https://update.greasyfork.org/scripts/421528/auto%20updating%20eruda%20toolkit%20%28kiwibrowser%20%20adguard%20Mobile%20Devtools%20%29.user.js
+// @updateURL https://update.greasyfork.org/scripts/421528/auto%20updating%20eruda%20toolkit%20%28kiwibrowser%20%20adguard%20Mobile%20Devtools%20%29.meta.js
+// ==/UserScript==
+alert("cc");
